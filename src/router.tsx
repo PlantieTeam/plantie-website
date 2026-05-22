648ba900feb8c2a746ae928909f2b99f998394c9
= <=>
2026-05-22 08:20:19 +0300

    refactor: add assistant route

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,23 @@
 import { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/home";
+import AssistantPage from "./pages/assistant";
+
 import MainLayout from "./layouts/MainLayout";
+
 const Router: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route index element={<HomePage />}></Route>
-          <Route path="/app" element={<HomePage />}></Route>
-          
+          <Route index element={<HomePage />} />
+          <Route path="/app" element={<HomePage />} />
+          <Route path="/assistant" element={<AssistantPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 };
+
 export default Router;
